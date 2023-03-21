0ef026e2197339c947a93f2ada05b7a31192e9b2
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2023-03-21 20:54:00 +0530

    Added documentation and its basic dependency

---
+++ src/main/java/com/asr/experiment/query/dsl/example/config/OpenApiConfig.java
@@ -0,0 +1,13 @@
+package com.asr.experiment.query.dsl.example.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Query DSL Example",
+        version = "0.0.1-SNAPSHOT",
+        description = "Project to demonstrate the Query DSl and Web capabilities in a Spring-boot Application")
+)
+public class OpenApiConfig {
+}
--- src/main/java/com/asr/experiment/query/dsl/example/entity/AbstractBaseEntity.java
+++ src/main/java/com/asr/experiment/query/dsl/example/entity/AbstractBaseEntity.java
@@ -1,5 +1,7 @@
 package com.asr.experiment.query.dsl.example.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -31,17 +33,19 @@ import org.springframework.data.annotation.LastModifiedDate;
 @ToString(onlyExplicitlyIncluded = true)
 public class AbstractBaseEntity {
 
+  public static final String COLUMN_VERSION_NAME = "version";
+
   @ToString.Include
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "id", nullable = false)
   @JdbcTypeCode(SqlTypes.VARCHAR)
+  @Schema(description = "Unique id for the Entity", accessMode = AccessMode.READ_ONLY)
   private UUID id;
-
-  public static final String COLUMN_VERSION_NAME = "version";
 
   @Setter(AccessLevel.NONE)
   @Version
+  @Schema(description = "Latest version for the Entity", accessMode = AccessMode.READ_ONLY)
   @Column(name = COLUMN_VERSION_NAME)
   private Integer version;
 
@@ -49,11 +53,13 @@ public class AbstractBaseEntity {
   @Temporal(TemporalType.TIMESTAMP)
   @CreatedDate
   @Column(nullable = false, updatable = false)
+  @Schema(description = "Time when entity was created", accessMode = AccessMode.READ_ONLY)
   private Date createdDate;
 
   @Setter(AccessLevel.NONE)
   @Temporal(TemporalType.TIMESTAMP)
   @LastModifiedDate
+  @Schema(description = "Latest Time when Entity was updated", accessMode = AccessMode.READ_ONLY)
   private Date lastModifiedDate;
 
 }
